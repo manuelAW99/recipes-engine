109189cd7aea13b2bc4391a1d9680f715c4e948f
Manuel Vilas <mvilasvaliente@gmail.com>
2023-06-12 17:39:14 -0400

    Add parser for each dataset

---
+++ data/5KRecipes/parser.py
@@ -0,0 +1,23 @@
+from json import dump
+
+recipes_file = open("raw_data/classes_Recipes5k.txt", "r")
+recipes = recipes_file.readlines()
+
+ingredients_file = open("raw_data/ingredients_Recipes5k.txt", "r")
+ingredients = ingredients_file.readlines()
+
+ingredients_simpl_file = open("raw_data/ingredients_simplified_Recipes5k.txt", "r")
+ingredients_simpl = ingredients_simpl_file.readlines()
+
+recipes_file.close()
+ingredients_file.close()
+ingredients_simpl_file.close()
+
+recipes_with_ing = {}
+for rs, ing, ings in zip(recipes, ingredients, ingredients_simpl):
+    recipes_with_ing[rs.replace("\n", "")] = {
+        "ingredients": [{"name": i.replace("\n", ""), "variants": []} for i in ing.split(",")],
+        "ingredients_simplified": [{"name": i.replace("\n", ""), "variants": []} for i in ings.split(",")]
+    }
+    
+dump(recipes_with_ing, open("parsed_data/recipes.json", "w"))
